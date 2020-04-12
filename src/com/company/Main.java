55e7437e34f7483b766bbce2b76ca14cb7522ac3
Ramona Tamas <monica.tamas1991@gmail.com>
2020-04-12 20:16:18 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,38 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Ramona");
+        byte myByte = 127;
+         int myInt = 32325;
+                        int resultat = myByte + myInt;
+                                System.out.println(resultat);
+          myInt = 8888;
+         short myShort = 44;
+          int resultat1 = myInt/ myShort;
+          System.out.println(resultat1);
+
+          int x = -5;
+          int y = 8;
+          int z = 6;
+          int resultat2 = x + y*z;
+            System.out.println(resultat2);
+
+
+        int x1 = 55;
+        int y1 = 9;
+        int resultat3 = (x1 + y1)%y1;
+        System.out.println(resultat3);
+
+float C=20+ -3*5/8;
+System.out.println(C);
+
+float D=5+15/3*2-8%3;
+System.out.print(D);
+
+        // write your code here
+
+    }
+}

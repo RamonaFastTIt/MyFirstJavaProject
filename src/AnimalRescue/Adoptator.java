d12e335f59e5eef6ea0411eecc3fe6e21524dfb2
Ramona Tamas <monica.tamas1991@gmail.com>
2020-04-20 14:12:36 +0300

    Tema2.java-Metode si Obiecte

---
+++ src/AnimalRescue/Adoptator.java
@@ -0,0 +1,18 @@
+package AnimalRescue;
+
+public class Adoptator {
+    public String name = "Mya";
+    public int age = 28;
+    public double money = 32.455;
+
+public void buy(){
+    System.out.println("Buying");
+}
+public void save(){
+    System.out.println("Saving..");}
+    public void spend(){
+        System.out.println("Spending..");
+    }
+
+
+}

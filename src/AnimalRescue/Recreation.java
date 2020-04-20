7923f3b58ca307c27a35fc3ca170329aef8b66ca
Ramona Tamas <monica.tamas1991@gmail.com>
2020-04-20 14:13:19 +0300

    Tema2.java-Metode si Obiecte

---
+++ src/AnimalRescue/Recreation.java
@@ -0,0 +1,14 @@
+package AnimalRescue;
+
+public class Recreation {
+    public String recreation = "Run, Play, Sleep";
+    public void walk (){
+        System.out.println("walking..");
+    }
+    public void pet(){
+        System.out.println("Petting..");
+    }
+    public void play(){
+        System.out.println("Playing..");
+    }
+}

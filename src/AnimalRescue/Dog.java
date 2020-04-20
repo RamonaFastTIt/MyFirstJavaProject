27157609f21924d42caad310b1e02e9b32ed58ce
Ramona Tamas <monica.tamas1991@gmail.com>
2020-04-20 13:53:56 +0300

    Tema2.java-Metode si Obiecte

---
+++ src/AnimalRescue/Dog.java
@@ -0,0 +1,28 @@
+package AnimalRescue;
+
+public class Dog {
+    public String name = "Tina";
+    public int age = 10;
+    public String color = "Brown";
+    public String race = "Basset";
+    public float weight = 20.5F;
+    public int levelOfHealth = 10;
+    public int feelingHungry = 10;
+    public int levelMood = 10;
+    public String food = "Pork";
+    public String recreation = "Bark";
+
+        public void eat() {
+        System.out.println("Eating.."); }
+        public void run(){
+            System.out.println("Running..");
+        }
+        public void bark(){
+            System.out.println("HamHam");
+        }
+    public void sleep(){
+        System.out.println("ZzZZ");
+    }
+
+
+}

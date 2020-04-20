140ff5d45ee49a89b8a473a5172e8cc596b74880
Ramona Tamas <monica.tamas1991@gmail.com>
2020-04-20 14:12:56 +0300

    Tema2.java-Metode si Obiecte

---
+++ src/AnimalRescue/AnimalFood.java
@@ -0,0 +1,17 @@
+package AnimalRescue;
+
+public class AnimalFood {
+    public String name = "Husse";
+    public int price = 37;
+    public int quantity = 2;
+    public int stock = 12;
+public void producer(){
+    System.out.println("Producing..");}
+    public void distribute(){
+        System.out.println("Distributing..");
+    }
+
+
+
+
+}

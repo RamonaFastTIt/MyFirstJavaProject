f24f14daf320a0d32fe24d2bc211be38f380dff7
Ramona Tamas <monica.tamas1991@gmail.com>
2020-04-20 14:13:45 +0300

    Tema2.java-Metode si Obiecte

---
+++ src/AnimalRescue/VetDoctor.java
@@ -0,0 +1,12 @@
+package AnimalRescue;
+
+public class VetDoctor {
+    public String name = "Ho Go Vet";
+    public String specialization = "General, Surgery, ";
+    public void consult(){
+        System.out.println("Consulting..");}
+        public void havingSurgery(){
+            System.out.println("Having...");
+        }
+
+}

59bf592a9b2d055cefbe8297bcd9ba55c63a3541
Ramona Tamas <monica.tamas1991@gmail.com>
2020-04-23 17:57:53 +0300

    lab3 encapsulare

--- src/AnimalRescue/Recreation.java
+++ src/AnimalRescue/Recreation.java
@@ -2,6 +2,15 @@ package AnimalRescue;
 
 public class Recreation {
     public String recreation = "Run, Play, Sleep";
+
+    public String getRecreation(){
+        return recreation;
+    }
+
+    public void setRecreation(String recreation) {
+        this.recreation = recreation;
+    }
+
     public void walk (){
         System.out.println("walking..");
     }

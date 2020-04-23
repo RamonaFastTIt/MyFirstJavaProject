d4b478a2d0c12b882f5cde03de4f6597d91dcd76
Ramona Tamas <monica.tamas1991@gmail.com>
2020-04-23 17:58:21 +0300

    lab3 encapsulare

--- src/AnimalRescue/VetDoctor.java
+++ src/AnimalRescue/VetDoctor.java
@@ -3,6 +3,22 @@ package AnimalRescue;
 public class VetDoctor {
     public String name = "Ho Go Vet";
     public String specialization = "General, Surgery, ";
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getSpecialization(){
+        return specialization;
+    }
+
+    public void setSpecialization(String specialization) {
+        this.specialization = specialization;
+    }
+
     public void consult(){
         System.out.println("Consulting..");}
         public void havingSurgery(){

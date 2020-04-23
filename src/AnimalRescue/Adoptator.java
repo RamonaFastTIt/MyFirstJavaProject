25820064e4f5086729adbad49b6ef831d8d5209e
Ramona Tamas <monica.tamas1991@gmail.com>
2020-04-23 17:57:04 +0300

    lab3 encapsulare

--- src/AnimalRescue/Adoptator.java
+++ src/AnimalRescue/Adoptator.java
@@ -1,11 +1,33 @@
 package AnimalRescue;
 
 public class Adoptator {
-    public String name = "Mya";
-    public int age = 28;
-    public double money = 32.455;
+    private String name;
+    private int age;
+    private double money;
 
-public void buy(){
+    public String getName() {
+        return name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public int getAge(){
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    public void buy(){
     System.out.println("Buying");
 }
 public void save(){
